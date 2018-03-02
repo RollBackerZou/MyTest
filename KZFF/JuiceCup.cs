49255553bef35f5e9fd5ca4d4703682100e9d7b5
Zou <zdc930818@163.com>
2018-03-02 17:18:30 +0800

    扩展方法

---
+++ KZFF/JuiceCup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KZFF
+{
+   public class JuiceCup
+    {
+       public JuiceCup()
+       {
+           Items = new List<string>();
+       }
+       public List<string> Items { get; set; }
+    }
+}
---
+++ KZFF/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KZFF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JuiceCup cup = new JuiceCup();
+            cup.Add("白开水");
+            cup.Items.ForEach(m => {
+                Console.WriteLine(m);
+            });
+            Console.ReadKey();
+        }
+    }
+    public static class KZ
+    {
+        public static List<string> Add(this JuiceCup cup,string water)
+        {
+            cup.Items.Add(water);
+            return cup.Items;
+        }
+    }
+}
